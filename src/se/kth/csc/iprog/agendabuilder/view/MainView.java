cba99e73d31ec1f86cf478b58dedabe5fb16a661
Ömer Sinan Şahin <sinanomer@hotmail.com>
2015-03-09 08:16:01 +0100

    Add a Day çalışınca yan tarafda yeni panel falan oluşturuyor. Benim eclipse biraz arızalı. O yüzden ne kadar iyi çalışıyor pek göremiyorum. Ayrıca yeni aktivite gösterimini yapamadım(10 min Introduction kısmı).

--- src/se/kth/csc/iprog/agendabuilder/view/MainView.java
+++ src/se/kth/csc/iprog/agendabuilder/view/MainView.java
@@ -10,12 +10,16 @@ import java.awt.event.ActionEvent;
 import javax.swing.border.LineBorder;
 
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
 
 import javax.swing.JLabel;
 
 import java.awt.Font;
 
 import javax.swing.JTextField;
+
+import java.awt.Canvas;
 
 public class MainView extends JPanel {
 	private JTextField textField;
@@ -32,6 +36,15 @@ public class MainView extends JPanel {
 		panel.setBorder(new LineBorder(new Color(0, 0, 0)));
 		panel.setBounds(10, 71, 155, 362);
 		add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblMin = new JLabel("10 min");
+		lblMin.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblMin.setBounds(10, 11, 46, 14);
+		panel.add(lblMin);
+
+		
+		
 		
 		JLabel lblStartTime = new JLabel("Start Time:");
 		lblStartTime.setFont(new Font("Tahoma", Font.PLAIN, 13));
@@ -86,7 +99,7 @@ public class MainView extends JPanel {
 				frame.setVisible(true);
 				frame.setResizable(false);
 				//frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				frame.add(p);
+				frame.getContentPane().add(p);
 			}
 		});
 		addActButton.setBounds(10, 37, 155, 23);
@@ -97,43 +110,52 @@ public class MainView extends JPanel {
 	
 	public void addNewDay()
 	{
-		JLabel lblStartTime = new JLabel("Start Time:");
-		lblStartTime.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		lblStartTime.setBounds(335, 47, 82, 23);
-		add(lblStartTime);
+		JLabel lblStartTime2 = new JLabel("Start Time:");
+		lblStartTime2.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblStartTime2.setBounds(355, 47, 82, 23);
+		add(lblStartTime2);
 		
-		textField = new JTextField(" 08:00 ");
-		textField.setBounds(211, 49, 40, 20);
-		add(textField);
-		textField.setColumns(10);
+		JTextField textField2 = new JTextField(" 08:00 ");
+		textField2.setBounds(431, 49, 40, 20);
+		add(textField2);
+		textField2.setColumns(10);
 		
 		JLabel label = new JLabel("End Time:");
 		label.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		label.setBounds(335, 71, 82, 23);
+		label.setBounds(355, 71, 82, 23);
 		add(label);
 		
 		JLabel lblNewLabel = new JLabel(" 08:00 ");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		lblNewLabel.setBounds(405, 75, 46, 14);
+		lblNewLabel.setBounds(425, 75, 46, 14);
 		add(lblNewLabel);
 		
 		JLabel label_1 = new JLabel("Total Length:");
 		label_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		label_1.setBounds(335, 92, 82, 23);
+		label_1.setBounds(355, 92, 82, 23);
 		add(label_1);
 		
 		JLabel label_2 = new JLabel(" 0 min ");
 		label_2.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		label_2.setBounds(315, 97, 46, 14);
+		label_2.setBounds(435, 97, 46, 14);
 		add(label_2);
+		
+		remove(addDayButton);
 		
 		JPanel panel_1 = new JPanel();
 		panel_1.setBorder(new LineBorder(new Color(0, 0, 0)));
-		panel_1.setBounds(335, 126, 155, 306);
+		panel_1.setBounds(355, 126, 155, 306);
 		add(panel_1);
 		
 		addDayButton = new JButton("+ Add a Day");
-		addDayButton.setBounds(510, 256, 120, 69);
+		addDayButton.setBounds(530, 256, 120, 69);
 		add(addDayButton);
+	}
+	
+	
+	public void paintComponent(Graphics g)
+	{
+		g.repaint();
+		
 	}
 }
